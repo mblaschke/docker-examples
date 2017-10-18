f683890248c2474236c6e0fe4b271f92a6bc6d49
Markus Blaschke <mblaschke82@gmail.com>
2017-10-18 20:25:18 +0200

    Add version to http-server

--- http-server/main.go
+++ http-server/main.go
@@ -14,10 +14,14 @@ var (
 	logger *log.Logger 
 )
 
+const (
+	VERSION = "1.0"
+)
+
 func hello(w http.ResponseWriter, r *http.Request) {
 	hostname, _ := os.Hostname()
 	currentDate := time.Now().Format(time.RFC850)
-	io.WriteString(w, fmt.Sprintf("Hello world! I'm %s and it's %s", hostname, currentDate))
+	io.WriteString(w, fmt.Sprintf("Hello world! I'm %s in version %s and it's %s", hostname, VERSION, currentDate))
 
 	ip, _, err := net.SplitHostPort(r.RemoteAddr)
 	if err == nil {
@@ -29,7 +33,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 func main() {
 	logger = log.New(os.Stdout, "logger: ", log.Lshortfile)
 
-	fmt.Println("Starting go http server...")
+	fmt.Println(fmt.Sprintf("Starting go http server version %s...", VERSION))
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", hello)
 	http.ListenAndServe(":80", mux)
